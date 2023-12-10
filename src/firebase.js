63420ba9b74babbd660665abe6ce0e1d7f3b7497
abebawmk <135061977+abebawmk@users.noreply.github.com>
2023-12-10 14:22:25 -0800

    my Netflix-clone project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBE41xP9lzXrd09xdIoGc5d0ZdebuGzR8U",
+  authDomain: "netflix-clone-6ef69.firebaseapp.com",
+  projectId: "netflix-clone-6ef69",
+  storageBucket: "netflix-clone-6ef69.appspot.com",
+  messagingSenderId: "649535136470",
+  appId: "1:649535136470:web:2cc623200caf33e62b1f77",
+};
---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "f5582e91630c95d87566c13b9cee8034";
+
+const requests = {
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
